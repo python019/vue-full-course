b3e01579fdbe2ae0c1ba6730fd1b395c2c758250
python019 <tommyjumayev@gmail.com>
2022-06-01 15:47:10 +0500

    base

--- application.js
+++ application.js
@@ -4,12 +4,16 @@ const app =  Vue.createApp({
         return {
             ism: 'Ubaydullo',
             familiya: 'Jumayev',
-            son: 0
+            son: 0,
+            qiymat: true
           }
     },
     methods: {
         IsmOzgartirish() {
             this.ism = "Jamshid"
+        },
+        qiymatniOzgartirish() {
+            this.qiymat = !this.qiymat
         }
     }
 
