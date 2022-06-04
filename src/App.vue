962bc4cebc004721e0724e654fed4f6bcf2661c7
python019 <tommyjumayev@gmail.com>
2022-06-05 00:36:47 +0500

    base

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Asosiy</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
