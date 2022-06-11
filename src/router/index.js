17004b1441fa01a74918fa36e3d069cfdc373074
python019 <tommyjumayev@gmail.com>
2022-06-12 03:41:21 +0500

    base

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AcademyView from '../views/AcademyView.vue'
+
 
 const routes = [
   {
@@ -14,6 +16,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/academy',
+    name: 'academy',
+    component: AcademyView
   }
 ]
 
---
+++ src/views/AcademyView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="academy">
+    <h1>Bu Academy View</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'AcadenyView',
+}
+</script>
+
+<style scoped>
+h1 {
+  color: crimson;
+}
+</style>
