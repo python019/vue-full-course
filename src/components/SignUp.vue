d0a526f7d04487c29a17ff54d79191d5979d7534
python019 <tommyjumayev@gmail.com>
2022-06-20 04:16:25 +0500

    main

--- src/components/SignUp.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <form action="">
-        <label>Email:</label>
-        <input type="text" required>
-        <br><br>
-        <label>Parol:</label>
-        <input type="password" required>
-    </form>
-</template>
