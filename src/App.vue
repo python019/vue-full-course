e43da43e2f3932518d259cf52f9245ea1e4924f0
python019 <tommyjumayev@gmail.com>
2022-06-20 04:15:00 +0500

    base

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <SignUp />
+</template>
+
+<script>
+import SignUp from './components/SignUp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SignUp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
