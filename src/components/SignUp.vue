4ca8a5003a54402c1070caa8397386458ad10c10
python019 <tommyjumayev@gmail.com>
2022-06-20 04:42:44 +0500

    v-model form

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,9 +1,23 @@
 <template>
     <form action="">
         <label>Email:</label>
-        <input type="text" required>
+        <input type="text" v-model="email" required>
         <br><br>
         <label>Parol:</label>
-        <input type="password" required>
+        <input type="password" v-model="pass" required>
     </form>
+
+    <h1>Sizning emailingiz: {{ email }}</h1>
+    <h1>Sizning parolingiz: {{ pass }}</h1>
 </template>
+
+<script>
+export default {
+   data () {
+        return{
+            email: '',
+            pass: ''
+        }
+   }
+}
+</script>
